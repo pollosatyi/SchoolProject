ebf703954cadf9064e6314b6e77b267056e491cc
pollosatyi <pollosatyi@mail.ru>
2024-12-25 21:21:53 +0300

    PrintSchhoolMenu готово

--- SchoolProject/Menu/PrintSchoolMenu.cs
+++ SchoolProject/Menu/PrintSchoolMenu.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SchoolProject.Enums;
+using SchoolProject.Print;
+using SchoolProject.Schools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,34 @@ using System.Threading.Tasks;
 
 namespace SchoolProject.Menu
 {
-    internal class PrintSchoolMenu
+    internal static class PrintSchoolMenu
     {
+        public static void PrintGroupMenuChoice(School school)
+        {
+            bool IsOnMenu = true;
+            while (IsOnMenu)
+            {
+                Print();
+                MenuPrintInSchoolType menuPrintInSchoolType = (MenuPrintInSchoolType)Convert.ToInt32(Console.ReadLine());
+                switch (menuPrintInSchoolType)
+                {
+                    case MenuPrintInSchoolType.exit:
+                        IsOnMenu = false;
+                        break;
+                    case MenuPrintInSchoolType.printStudentsInSchool:
+                        PrintSchoolElements.PrintStudents(school);
+                        break;
+
+                }
+
+            }
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine("Меню выбора окна для печати группы");
+            Console.WriteLine("0 - для выхода \n" +
+                "1 - для печати списка студентов");
+        }
     }
 }
